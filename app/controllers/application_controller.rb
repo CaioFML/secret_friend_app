2ffa57b8278fee6c7029f3805fc77c6158a5e0c8
CaioFML <caio.felipemotta@hotmail.com>
2020-02-18 20:23:35 -0300

    Setando o retorno do 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+  private
+
+  def render_404
+    redirect_to main_app.root_url
+  end
 end
